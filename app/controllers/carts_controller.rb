00dc427d5dc9060052b2dfbfe2c5f4968dc52a73
phil-a <phil.alekseev@gmail.com>
2015-07-27 23:01:03 -0400

    Add Cart total and incorrect cart url redirection

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,74 +1,106 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
+#---
+# Excerpted from "Agile Web Development with Rails, 4rd Ed.",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 class CartsController < ApplicationController
-  before_action :set_cart, only: [:show, :edit, :update, :destroy]
-
   # GET /carts
-  # GET /carts.json
+  # GET /carts.xml
   def index
     @carts = Cart.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @carts }
+    end
   end
 
   # GET /carts/1
-  # GET /carts/1.json
+  # GET /carts/1.xml
   def show
-  end
-
-  # GET /carts/new
-  def new
-    @cart = Cart.new
-  end
-
-  # GET /carts/1/edit
-  def edit
-  end
-
-  # POST /carts
-  # POST /carts.json
-  def create
-    @cart = Cart.new(cart_params)
-
-    respond_to do |format|
-      if @cart.save
-        format.html { redirect_to @cart, notice: 'Cart was successfully created.' }
-        format.json { render :show, status: :created, location: @cart }
-      else
-        format.html { render :new }
-        format.json { render json: @cart.errors, status: :unprocessable_entity }
+    begin
+      @cart = Cart.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, :notice => 'Invalid cart'
+    else
+      respond_to do |format|
+        format.html # show.html.erb
+        format.xml  { render :xml => @cart }
       end
     end
   end
 
-  # PATCH/PUT /carts/1
-  # PATCH/PUT /carts/1.json
-  def update
+  # GET /carts/new
+  # GET /carts/new.xml
+  def new
+    @cart = Cart.new
+
     respond_to do |format|
-      if @cart.update(cart_params)
-        format.html { redirect_to @cart, notice: 'Cart was successfully updated.' }
-        format.json { render :show, status: :ok, location: @cart }
+      format.html # new.html.erb
+      format.xml  { render :xml => @cart }
+    end
+  end
+
+  # GET /carts/1/edit
+  def edit
+    @cart = Cart.find(params[:id])
+  end
+
+  # POST /carts
+  # POST /carts.xml
+  def create
+    @cart = Cart.new(params[:cart])
+
+    respond_to do |format|
+      if @cart.save
+        format.html { redirect_to(@cart, :notice => 'Cart was successfully created.') }
+        format.xml  { render :xml => @cart, :status => :created, :location => @cart }
       else
-        format.html { render :edit }
-        format.json { render json: @cart.errors, status: :unprocessable_entity }
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @cart.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /carts/1
+  # PUT /carts/1.xml
+  def update
+    @cart = Cart.find(params[:id])
+
+    respond_to do |format|
+      if @cart.update_attributes(params[:cart])
+        format.html { redirect_to(@cart, :notice => 'Cart was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @cart.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # DELETE /carts/1
-  # DELETE /carts/1.json
+  # DELETE /carts/1.xml
   def destroy
+    @cart = current_cart
     @cart.destroy
+    session[:cart_id] = nil
+
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to(store_url,
+        :notice => 'Your cart is currently empty') }
+      format.xml  { head :ok }
     end
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_cart
-      @cart = Cart.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def cart_params
-      params[:cart]
-    end
 end
